44e77eac1e13fc2dfa4c0e1793d81eeceeb41c89
Lifebrushed <Dragongameralex@gmail.com>
2026-03-15 12:50:08 +0800

    added tags and renamed a few things for further development

---
+++ Main/Lifebrushed/SourceCode/Entities.py
@@ -0,0 +1,37 @@
+import Dnd
+import Tags
+
+class CreatureBase() : # guys no one fucking touch this if you touch this the game will break
+    def __init__(self, name="placeholder_name", str=0, dex=0, con=0, wil=0, int=0, cha=0, align=Tags.TN):
+        self.name = name
+        self.description = self.getDescription
+        self.strength = str
+        self.dexterity = dex
+        self.constitution = con
+        self.willpower = wil
+        self.intelligence = int
+        self.charisma = cha
+        self.level = 1
+        self.maxhealth = self.getMaxHealth()
+        self.health = self.maxhealth
+        self.ac = self.getArmorClass()
+        self.alignment = align
+
+    def getMaxHealth(self):
+        MaxHealth = Dnd.d12(self.level) + self.getStatBonus(self.constitution)
+        return MaxHealth
+    
+    def TakeDamage(self, damage):
+        self.health = self.health - damage
+
+    def getArmorClass(self):
+        AC = 10 + self.getStatBonus(self.constitution)
+        return AC
+    
+    def getStatBonus(self, stat):
+        StatBonus = (stat - 10)//2
+        return StatBonus
+    
+    def getDescription(self):
+        description = "the description was too long to put in the __init__ so i made it a function"
+        return description
---
+++ Main/Lifebrushed/SourceCode/Tags.py
@@ -0,0 +1,28 @@
+# applicable tags onto stuff
+
+#character alignments
+
+LG = "lawful_good"
+NG = "neutral_good"
+CG = "chaotic_good"
+
+LN = "lawful_neutral"
+TN = "true_neutral"
+CN = "chaotic_neutral"
+
+LE = "lawful_evil"
+NE = "neutral_evil"
+CE = "chaotic_evil"
+
+# elemental proccing
+
+PYRE = "pyreweave"
+TIDE = "tidetwine"
+TEMPEST = "tempestcoil"
+STONE = "stonebind"
+
+# physical dmg types
+
+BLUNT = "blunt"
+SLASH = "slashing"
+PIERCE = "piercing"
