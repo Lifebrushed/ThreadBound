cb3c5ee00058b4454806ea7b309f296ab11b3d2f
Lifebrushed <Dragongameralex@gmail.com>
2026-03-16 08:46:23 +0800

    Update ItemCore.py

--- Main/Lifebrushed/SourceCode/Items/ItemCore.py
+++ Main/Lifebrushed/SourceCode/Items/ItemCore.py
@@ -1,5 +1,7 @@
+import logic.tags as tags
+
 class item():
-   def __init__(self, name="placehodler name", rarity):
+   def __init__(self, name="placeholder name", rarity=tags.COMMON):
       self.name = name
       self.rarity = rarity
       
