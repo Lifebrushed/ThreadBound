dd26d250f772599d5575c2df1f13315cb970c68a
Lifebrushed <Dragongameralex@gmail.com>
2026-03-16 15:37:11 +0800

    Update ItemCore.py

--- Main/Lifebrushed/SourceCode/Items/ItemCore.py
+++ Main/Lifebrushed/SourceCode/Items/ItemCore.py
@@ -1,8 +1,9 @@
 import Logic.Tags as tags
 
-class item(): #ikm t going insane please i beg you i need someone else to help me w/ my project
+class item(): #istg i'm going insane please i beg you i need someone else to help me w/ this project
    def __init__(self, name="placeholder_name_item", rarity=tags.COMMON):
       self.name = name
+      self.description = self.getDescription()
       self.rarity = rarity
    
    def isEnchantable(self): #check if its enchantable
@@ -12,11 +13,14 @@ class item(): #ikm t going insane please i beg you i need someone else to help m
       return true
    
    def getTags(self): # use this when u want to proc certain talents!
-      Tags = [
+      tags = [
       
       ]
-      return Tags
+      return tags
+   
+   def getDescription(self):
+      desc = "this is a placeholder description!"
+      return desc
       
-   def 
    
    
