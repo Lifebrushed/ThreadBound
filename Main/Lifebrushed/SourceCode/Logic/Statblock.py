ee201171ee5e47be18714310896fcf7bacef7b0a
Lifebrushed <Dragongameralex@gmail.com>
2026-03-17 15:27:37 +0800

    Create Statblock.py

---
+++ Main/Lifebrushed/SourceCode/Logic/Statblock.py
@@ -0,0 +1,17 @@
+class statblock():
+    def __init__(self, dex=1, str=1, con=1, int=1, wil=1, cha=1, pyr=0, tde=0, tmp=0, stn=0):
+        self.dexterity = dex
+        self.strength = str
+        self.constitution = con
+        self.intelligence = int
+        self.willpower = wil
+        self.charisma = cha
+        
+        self.stonebind = stn
+        self.pyreweave = pyr
+        self.tidetwine = tde
+        self.tempestcoil = tmp
+        
+        
+        
+        
